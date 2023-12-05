a6d38883b52b90bf84ed92ef236015139f98eae4
Munazza Javed <munazzajaved277@gmail.com>
2023-12-05 20:50:18 +0500

    Functions to compute the three sepia-tone

--- colorUtils.c
+++ colorUtils.c
@@ -28,6 +28,24 @@ int toGrayScaleLuminosity(int r, int g, int b){
   return round(0.21*r + 0.72*g + 0.07*b);
 }
 
+// three functions to compute the three sepia-tone RGB values
+int toSepiaRed(int r, int g, int b){
+  // for red 0.393r + 0.769g + 189b;
+  return round(0.393*r + 0.769*g + 189*b);
+}
+
+int toSepiaGreen(int r, int g, int b){
+  // for green 0.349r + 0.686g + 0.131b;
+  return round(0.349*r + 0.686*g + 0.131*b);
+}
+
+int toSepiaBlue(int r, int g, int b){
+  // for blue 0.272r + 0.534g + 0.131b;
+  return round(0.272*r + 0.534*g + 0.131*b);
+}
+
+
+
 int toGrayScaleAverage(int r, int g, int b) {
   //TODO: test this, it may be wrong!
   return (r + g + b) / 3;
