b5c7b67b57949306a0ecc1548949e2dfa0f86f32
Munazza Javed <munazzajaved277@gmail.com>
2023-12-05 20:32:33 +0500

    Function toGrayScaleLuminosity

--- colorUtils.c
+++ colorUtils.c
@@ -1,4 +1,5 @@
-
+#include <stdio.h>
+#include <math.h>
 #include "colorUtils.h"
 
 double rgbIntToFloat(int c) {
@@ -20,6 +21,11 @@ int min(int x,int y,int z){
 int toGrayScaleLightness(int r, int g, int b){
   // lightness technique (max r,g,b + min r,g,b)/2;
   return (max(r,g,b) + min(r,g,b))/2;
+}
+
+int toGrayScaleLuminosity(int r, int g, int b){
+  // luminousty technique 0.21r + 0.72g + 0.07b;
+  return round(0.21*r + 0.72*g + 0.07*b);
 }
 
 int toGrayScaleAverage(int r, int g, int b) {
